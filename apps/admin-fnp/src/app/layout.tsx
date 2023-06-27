1775e506f96aca7dc94f85b3158b070560d5e817
okandas <raykandas@gmail.com>
2023-06-27 15:56:45 +0200

    refactor: metadata

--- apps/admin-fnp/src/app/layout.tsx
+++ apps/admin-fnp/src/app/layout.tsx
@@ -5,8 +5,8 @@ import { Inter } from "next/font/google"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
-  title: "farmnport",
-  description: "Getting you to market",
+  title: "farmnport - admin",
+  description: "Manage Farmnport",
 }
 
 export default function RootLayout({
