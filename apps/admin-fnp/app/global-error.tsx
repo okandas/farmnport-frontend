62e6baf978f4be659123fb6b03780a6d65314d85
okandas <okandasjr@gmail.com>
2024-01-23 23:26:53 +0200

    chore: remove NextError component not working

--- apps/admin-fnp/app/global-error.tsx
+++ apps/admin-fnp/app/global-error.tsx
@@ -1,28 +1,30 @@
-"use client";
+"use client"
 
-import * as Sentry from "@sentry/nextjs";
-import NextError from "next/error";
-import { useEffect } from "react";
+import * as Sentry from "@sentry/nextjs"
+import { useEffect } from "react"
 
 interface GlobalErrorProps {
     error: Error & { digest?: string },
-    children: React.ReactNode
+    reset: () => void
 }
 
-export default function GlobalError({ error, children }: GlobalErrorProps) {
+export default function GlobalError({ error, reset }: GlobalErrorProps) {
 
     useEffect(() => {
         Sentry.captureException(error);
     }, [error]);
 
     return (
-        <html>
-            <body>
-                {/* This is the default Next.js error component but it doesn't allow omitting the statusCode property yet. */}
-                <NextError statusCode={undefined as any}>
-                    {children}
-                </NextError>
-            </body>
-        </html>
+        <div>
+            <h2>Something went wrong!</h2>
+            <button
+                onClick={
+                    // Attempt to recover by trying to re-render the segment
+                    () => reset()
+                }
+            >
+                Try again
+            </button>
+        </div>
     );
 }
