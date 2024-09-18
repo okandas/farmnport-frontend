2b2e6eecb22317052d942827c695e62984abc1f1
okandas <okandasjr@gmail.com>
2024-09-18 15:26:19 +0200

    chore: update sentry to add source maps

--- apps/client-fnp/sentry.client.config.ts
+++ apps/client-fnp/sentry.client.config.ts
@@ -1,26 +1,28 @@
-
 // This file configures the initialization of Sentry on the client.
 // The config you add here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import { init } from "@sentry/nextjs"
+import * as Sentry from "@sentry/nextjs";
 
-init({
-    dsn: "https://106493b0d84747b9b484020fe4c1c8a9@o144609.ingest.sentry.io/1212181",
+Sentry.init({
+  dsn: "https://106493b0d84747b9b484020fe4c1c8a9@o144609.ingest.us.sentry.io/1212181",
 
-    // Adjust this value in production, or use tracesSampler for greater control
-    tracesSampleRate: 1,
+  // Add optional integrations for additional features
+  integrations: [
+    Sentry.replayIntegration(),
+  ],
 
-    // Setting this option to true will print useful information to the console while you're setting up Sentry.
-    debug: false,
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
 
-    replaysOnErrorSampleRate: 1.0,
+  // Define how likely Replay events are sampled.
+  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // in development and sample at a lower rate in production
+  replaysSessionSampleRate: 0.1,
 
-    // This sets the sample rate to be 10%. You may want this to be 100% while
-    // in development and sample at a lower rate in production
-    replaysSessionSampleRate: 0.1,
+  // Define how likely Replay events are sampled when an error occurs.
+  replaysOnErrorSampleRate: 1.0,
 
-    // You can remove this option if you're not planning to use the Sentry Session Replay feature:
-    integrations: [
-    ],
-})
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
