ad77c38f2e21c9071b68a53cdee81b7b4316d4c0
okandas <okandas@bara.co.zw>
2025-04-15 16:57:00 +0200

    fix: use correct logger

--- apps/client-fnp/lib/logger.ts
+++ apps/client-fnp/lib/logger.ts
@@ -1,4 +1,4 @@
-import {Logtail} from "@logtail/node";
+import {Logtail} from "@logtail/browser";
 
 export const logtail = new Logtail("CYbNREBB4bygwzdf1pW6LP5y", {
   endpoint: 'https://s1275003.eu-nbg-2.betterstackdata.com',
