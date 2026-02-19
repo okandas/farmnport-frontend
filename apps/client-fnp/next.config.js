cf76aa1dc0e48ae9776e41413d64d36ddd83bca5
okandas <okandas@bara.co.zw>
2026-02-19 14:47:09 +0200

    fix: update image hostname to images.farmnport.com and add logos pattern

--- apps/client-fnp/next.config.js
+++ apps/client-fnp/next.config.js
@@ -6,15 +6,21 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 's3.farmnport.com',
+        hostname: 'images.farmnport.com',
         port: '',
         pathname: '/images/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.farmnport.com',
+        port: '',
+        pathname: '/logos/**'
       },
       {
         protocol: 'http',
         hostname: '127.0.0.1',
         port: '9000',
-        pathname: '/images/**'
+        pathname: '/**'
       },
     ],
   },
